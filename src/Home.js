6cfc26dfa0bcce50210ac90a5ad6ad38622c2973
hungapdo <111982815+hungapdo@users.noreply.github.com>
2022-12-02 19:24:15 +0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Home.css';
+export default function Home() {
+  return (   
+    <header className='header'>
+      <div className='header_inner'>
+        <div className='container'>
+          ::before
+          <div className='container'>
+            <div className='header__wrapper'>
+              <div className='menu-top'></div>
+              <a className='header_logo'></a>
+              <nav className='nav header_nav'></nav>
+              <div className='header_actions'></div>
+            </div>
+          </div>
+          ::after
+        </div>
+
+      </div>
+    </header>
+  )
+}
